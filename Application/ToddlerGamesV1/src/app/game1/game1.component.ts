b6cc2c2ba12004bc982574aae6174b433129d546
rnmehta1 <ria.mehta@asu.edu>
2021-03-14 12:53:53 -0700

    Make draggable list to test cdkDragAndDrop

--- Application/ToddlerGamesV1/src/app/game1/game1.component.ts
+++ Application/ToddlerGamesV1/src/app/game1/game1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
@@ -7,50 +7,34 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
   styleUrls: ['./game1.component.css']
 })
 
-export class Game1Component implements OnInit {
+export class Game1Component {
  title = 'Drag & Drop Shapes';
 
-todos = [
-    {
-      name: 'Angular',
-      category: 'Web Development'
-    },
-    {
-      name: 'Flexbox',
-      category: 'Web Development'
-    },
-    {
-      name: 'iOS',
-      category: 'App Development'
-    },
-    {
-      name: 'Java',
-      category: 'Software development'
-    }
+MoviesList = [
+    'Blade Runner',
+    'Cool Hand Luke',
+    'Heat',
+    'Juice',
+    'The Far Side of the World',
+    'Morituri',
+    'Napoleon Dynamite',
+    'Pulp Fiction'
   ];
 
-  completed = [
-    {
-      name: 'Android',
-      category: 'Mobile Development'
-    },
-    {
-      name: 'MongoDB',
-      category: 'Databases'
-    },
-    {
-      name: 'ARKit',
-      category: 'Augmented Reality'
-    },
-    {
-      name: 'React',
-      category: 'Web Development'
-    }
+ MoviesWatched = [
+ 'Ria'
   ];
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  onDrop(event: CdkDragDrop<string[]>) {
+      if (event.previousContainer === event.container) {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      } else {
+        transferArrayItem(event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex);
+      }
+    }
 }
 
