12e4258ac6b67bc9f17a44f8d43f4e1c9bbd756d
Suraj Suryawanshi <surajms0303@gmail.com>
2021-03-25 02:15:04 -0700

    updated game animations for fall and multiple bricks added

---
+++ Application/ToddlerGamesV1/src/app/game1/draws-scene.ts
@@ -0,0 +1,42 @@
+import { Image, Surface, Path, Text, Group, geometry } from '@progress/kendo-drawing';
+const { Rect, Point, Size, transform } = geometry;
+
+export function drawScene(surface: Surface) {
+  // Create the square border by drawing a straight path
+  const path = new Path({
+    stroke: {
+      color: "#9999b6",
+      width: 2
+    }
+  });
+
+  // The path is constructed by using a chain of commands
+  path.moveTo(0, 0)
+  .lineTo(150, 0).lineTo(150, 65).lineTo(0, 65)
+  .close();
+
+  // This rectangle defines the image position and size
+  const imageRect = new Rect(
+    new Point(5, 5),
+    new Size(50, 50)
+  );
+
+  // Create the text
+  const text = new Text(
+    "Diego Roel",
+    new Point(60, 25),
+    { font: "bold 15px Arial" }
+  );
+
+  // Place all the shapes in a group
+  const group = new Group();
+  group.append(path);
+
+  // Translate the group
+  group.transform(
+    transform().translate(50, 50)
+  );
+
+  // Render the group on the surface
+  surface.draw(group);
+}
